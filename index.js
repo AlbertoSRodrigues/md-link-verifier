0d46a2660f7860f3cd6239c12e2d75a116b0e4c6
AlbertoSRodrigues <albertobeto777@gmail.com>
2023-02-15 18:01:46 -0300

    Implementing Fs Function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs'
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Não há arquivo especificado'))
+}
+
+async function pegaArquivo(caminho){
+    try{
+        const texto = await fs.promises.readFile(caminho, "utf-8")
+             console.log(chalk.green(texto))
+    } catch(erro){
+        trataErro(erro)
+    }
+    }
+
+    pegaArquivo('./arquivos/text')
+   
+
